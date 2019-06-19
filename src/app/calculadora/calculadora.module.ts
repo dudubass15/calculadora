eae56e448379ed1439f37431a48b960ae013f910
dudubass15 <carlos.contato00@gmail.com>
2019-06-19 13:37:43 -0300

    subindo arquivos

---
+++ src/app/calculadora/calculadora.module.ts
@@ -0,0 +1,20 @@
+import { CalculadoraService } from './services/calculadora.service';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './components/calculadora.component';
+
+@NgModule({
+  declarations: [
+    CalculadoraComponent
+  ],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    CalculadoraComponent
+  ],
+  providers: [
+    CalculadoraService
+  ]
+})
+export class CalculadoraModule { }
